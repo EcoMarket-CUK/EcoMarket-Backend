87a0d51b61ba97bebed903a7ac984aab5cf7f206
hershyYJ <juj0478@naver.com>
2024-09-04 14:33:14 +0900

    [feature/#1] Initial commit

---
+++ src/main/java/com/api/jaebichuri/JaebichuriApplication.java
@@ -0,0 +1,13 @@
+package com.api.jaebichuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaebichuriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaebichuriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/jaebichuri/JaebichuriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.jaebichuri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaebichuriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
