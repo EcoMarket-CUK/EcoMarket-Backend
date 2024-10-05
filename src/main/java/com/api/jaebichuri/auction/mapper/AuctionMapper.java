841bc62ef831325681d416a26ce1085b8f0afc83
hershyYJ <juj0478@naver.com>
2024-10-05 20:22:02 +0900

    [feature/#9] Create auction mapper

--- src/main/java/com/api/jaebichuri/auction/mapper/AuctionMapper.java
+++ src/main/java/com/api/jaebichuri/auction/mapper/AuctionMapper.java
@@ -1,30 +1,46 @@
 package com.api.jaebichuri.auction.mapper;
 
-import com.api.jaebichuri.auction.dto.UpcomingAuctionProductDto;
-import com.api.jaebichuri.auction.dto.ProductResponseDto;
+import com.api.jaebichuri.auction.dto.ProductDto;
 import com.api.jaebichuri.auction.entity.Auction;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring")
 public interface AuctionMapper {
 
     AuctionMapper INSTANCE = Mappers.getMapper(AuctionMapper.class);
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @Mapping(source = "product.id", target = "productId")
     @Mapping(source = "product.productName", target = "productName")
     @Mapping(source = "product.productDescription", target = "productDescription")
-    @Mapping(source = "seller.nickname", target = "sellerNickname")
-    UpcomingAuctionProductDto auctionToUpcomingAuctionProductDto(Auction auction);
+    ProductDto toProductDto(Auction auction);
 
-    @Mapping(source = "product.id", target = "productId")
-    @Mapping(source = "product.productName", target = "productName")
-    @Mapping(source = "seller.nickname", target = "sellerNickname")
-    ProductResponseDto auctionToProductResponseDto(Auction auction);
+    default List<ProductDto> toOngoingProductDtoList(List<Auction> auctions) {
+        return auctions.stream()
+                .map(auction -> {
+                    ProductDto responseDto = toProductDto(auction);
+                    responseDto.setStartTime(null);
+                    responseDto.setEndTime(auction.getEndTime().format(formatter));
+                    return responseDto;
+                })
+                .collect(Collectors.toList());
+    }
 
-    List<ProductResponseDto> toProductResponseDtoList(List<Auction> auctions);
+    default List<ProductDto> toUpcomingProductDtoList(List<Auction> auctions) {
+        return auctions.stream()
+                .map(auction -> {
+                    ProductDto responseDto = toProductDto(auction);
+                    responseDto.setStartTime(auction.getStartTime().format(formatter));
+                    responseDto.setEndTime(null);
+                    return responseDto;
+                })
+                .collect(Collectors.toList());
+    }
 
 }
