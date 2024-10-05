80762e5484a98e3c2840b73cfbf5e45149085c15
hershyYJ <juj0478@naver.com>
2024-10-05 20:22:52 +0900

    [feature/#9] Create auction product service

--- src/main/java/com/api/jaebichuri/auction/service/ProductService.java
+++ src/main/java/com/api/jaebichuri/auction/service/ProductService.java
@@ -1,10 +1,9 @@
 package com.api.jaebichuri.auction.service;
 
 import com.api.jaebichuri.auction.dto.UpcomingAuctionProductDto;
-import com.api.jaebichuri.auction.dto.ProductResponseDto;
 import com.api.jaebichuri.auction.entity.Auction;
 import com.api.jaebichuri.auction.enums.AuctionStatus;
-import com.api.jaebichuri.auction.mapper.AuctionMapper;
+import com.api.jaebichuri.auction.mapper.ProductMapper;
 import com.api.jaebichuri.auction.repository.AuctionRepository;
 import com.api.jaebichuri.global.response.code.status.ErrorStatus;
 import com.api.jaebichuri.global.response.exception.CustomException;
@@ -12,30 +11,20 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final AuctionRepository auctionRepository;
-    private final AuctionMapper auctionMapper;
+    private final ProductMapper productMapper;
 
     @Transactional(readOnly = true)
-    public List<ProductResponseDto> getAllAuctionProducts() {
-        List<Auction> auctions = auctionRepository.findAll();
-
-        return auctionMapper.toProductResponseDtoList(auctions);
-    }
-
-    @Transactional(readOnly = true)
-    public UpcomingAuctionProductDto getUpcomingAuctionProductDetails(long auctionId) {
+    public UpcomingAuctionProductDto getUpcomingAuctionProductDetails(Long auctionId) {
         Auction auction = auctionRepository.findByIdAndAuctionStatus(auctionId, AuctionStatus.UPCOMING)
                 .orElseThrow(() -> new CustomException(ErrorStatus._AUCTION_NOT_FOUND));
 
-        return auctionMapper.auctionToUpcomingAuctionProductDto(auction);
+        return productMapper.auctionToUpcomingAuctionProductDto(auction);
     }
 
 }
