fced8c3b16a2d2b5edb256b5a585e8c3395654da
hershyYJ <juj0478@naver.com>
2024-10-05 20:22:26 +0900

    [feature/#9] Create auction repository

--- src/main/java/com/api/jaebichuri/auction/repository/AuctionRepository.java
+++ src/main/java/com/api/jaebichuri/auction/repository/AuctionRepository.java
@@ -1,15 +1,21 @@
 package com.api.jaebichuri.auction.repository;
 
 import com.api.jaebichuri.auction.entity.Auction;
+import com.api.jaebichuri.auction.enums.AuctionCategory;
 import com.api.jaebichuri.auction.enums.AuctionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AuctionRepository extends JpaRepository<Auction, Long> {
 
     Optional<Auction> findByIdAndAuctionStatus(long auctionId, AuctionStatus auctionStatus);
+
+    List<Auction> findTop5ByAuctionStatusOrderByStartTimeDesc(AuctionStatus auctionStatus);
+
+    List<Auction> findTop5ByAuctionStatusAndAuctionCategoryOrderByStartTimeDesc(AuctionStatus auctionStatus, AuctionCategory category);
 
 }
