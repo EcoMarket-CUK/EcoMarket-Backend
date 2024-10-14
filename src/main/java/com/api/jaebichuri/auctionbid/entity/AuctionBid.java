3ae6009c873e0ed777e32b41d91f9e314f07b6a1
veronees <jsg6854@naver.com>
2024-10-15 00:51:12 +0900

    [fix/#19] AuctionBid 패키지 분리

---
+++ src/main/java/com/api/jaebichuri/auctionbid/entity/AuctionBid.java
@@ -0,0 +1,35 @@
+package com.api.jaebichuri.auctionbid.entity;
+
+import com.api.jaebichuri.auction.entity.Auction;
+import com.api.jaebichuri.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AuctionBid {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long bidPrice;
+
+    @Column(nullable = false)
+    private LocalDateTime bidTime;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "auction_id", nullable = false)
+    private Auction auction;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member bidder;
+
+}
