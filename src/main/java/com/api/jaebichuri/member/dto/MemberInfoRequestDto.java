302447fef50a97b762e5e6f343c13454cf95e8c7
veronees <jsg6854@naver.com>
2024-11-13 20:53:28 +0900

    docs: Member 도메인 스웨거 문서 수정

--- src/main/java/com/api/jaebichuri/member/dto/MemberInfoRequestDto.java
+++ src/main/java/com/api/jaebichuri/member/dto/MemberInfoRequestDto.java
@@ -1,17 +1,23 @@
 package com.api.jaebichuri.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "사용자 추가 정보 입력 DTO")
 public class MemberInfoRequestDto {
 
     @NotBlank
+    @Schema(description = "이름", example = "김땡땡")
     private String name;
     @NotBlank
+    @Schema(description = "닉네임", example = "땡땡이")
     private String nickname;
     @NotBlank
+    @Schema(description = "우편번호", example = "12345")
     private String zipcode;
     @NotBlank
+    @Schema(description = "주소", example = "경기도 부천시 원미구 지봉로 43")
     private String address;
 }
